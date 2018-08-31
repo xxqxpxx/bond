077b82e4e1175a840008b14bdcb4943833e96bef
Ahmed Abdul Fatah <ahmedafatah@outlook.com>
2018-08-31 23:58:15 +0200

    strings

--- app/src/main/java/eg/com/cat/bond/MainActivity.java
+++ app/src/main/java/eg/com/cat/bond/MainActivity.java
@@ -1,13 +1,17 @@
 package eg.com.cat.bond;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
